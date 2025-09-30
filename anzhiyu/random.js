a2038f5008eaa90a99b98fdfd582b01308c1247d
Haotian Tu <HaotianTu@126.com>
2025-10-01 06:15:49 +0800

    Site updated: 2025-10-01 06:15:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/30/1-性价比超高的串串自助/"];function toRandomPost(){
+var posts=["2025/09/30/1-性价比超高的串串自助/","2025/10/01/2-10分钟打造自己的个人财务系统/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
